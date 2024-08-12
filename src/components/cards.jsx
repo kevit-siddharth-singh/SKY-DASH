4103d94cca84365bc37db57dbf1373e8458dcc8c
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-08-12 15:39:18 +0530

    card 2 UI Done

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -3,6 +3,8 @@ import React from "react";
 // Sid: Importing Icons
 import { IoMdPause } from "react-icons/io";
 import { MdNavigateNext } from "react-icons/md";
+import { FaStar } from "react-icons/fa";
+import { FaPlay } from "react-icons/fa";
 // Sid: Importing Icons
 
 // !Global Icons Size
@@ -45,25 +47,63 @@ const cards = () => {
             </div>
           </div>
         </div>
-        <div className="card2 w-full gray-round">
-          <div className="heading flex  justify-between  items-center">
+        <div className="card2 w-full gray-round p-4 flex flex-col gap-4">
+          <div
+            className="heading flex  justify-between  items-center 
+          mx-2"
+          >
             <p className="task-text">Today's tasks</p>
             <div className="manage-wrapper flex items-center">
               <p className="manage-text">Manage</p>
               <MdNavigateNext size={20} />
             </div>
           </div>
-          <div className="tasks flex  justify-center items-center"> 
-            <div className="task flex justify-around items-center w-full">
+          <div className="tasks flex  justify-center items-center  p-3 rounded-xl mx-2 border border-[#ff3c006c] bg-[#3231313e]">
+            <div className="task flex justify-around items-center w-full ">
               <div className="pause  flex  justify-center items-center">
-              <IoMdPause size={15} />
+                <IoMdPause size={20} />
               </div>
               <div className="content">
                 <p className="content-title">Color Palette Selection</p>
+                <p className="text-[#3859ed] text-xs text-start ">
+                  Transient Xero
+                </p>
               </div>
+              <FaStar />
             </div>
           </div>
-          
+
+          {/*Card 2 - Section 2  */}
+
+          <div className="tasks flex  justify-center items-center  p-3 rounded-xl mx-2  bg-[#3231313e]">
+            <div className="task flex justify-around items-center w-full ">
+              <div className="play ">
+                <FaPlay size={20} fill="#3859ed" />
+              </div>
+              <div className="content">
+                <p className="content-title">The sky is not the limit..</p>
+                <p className="text-[#3859ed] text-xs text-start">
+                  Team Dominik Sky
+                </p>
+              </div>
+              <FaStar />
+            </div>
+          </div>
+
+          {/*Card 2 - Section 3  */}
+
+          <div className="tasks flex  justify-center items-center  p-3 rounded-xl mx-2  bg-[#3231313e]">
+            <div className="task flex justify-around items-center w-full ">
+              <div className="play ">
+                <FaPlay size={20} fill="#3859ed" />
+              </div>
+              <div className="content ">
+                <p className="content-title">The Way down we go...</p>
+                <p className="text-[#3859ed] text-xs text-start">Xero Instincts</p>
+              </div>
+              <FaStar />
+            </div>
+          </div>
         </div>
         <div className="card3 w-full gray-round">hey</div>
       </div>
